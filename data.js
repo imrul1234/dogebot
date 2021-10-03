1455962e6bff69e6182ffe0a056d16d232806df8
imrul1234 <85102891+imrul1234@users.noreply.github.com>
2021-10-03 08:13:22 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://new_bot:Imrul@1234@cluster0.eynwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
+
+bot_token:'1975054943:AAGeyDPMvFv6jnxGDyaUw9wiKuwbm6hXbpI',//bot token
+
+bot_name:'Instantly_PayLTDRobot',//bot username without @
+
+bot_admin: 1370761717,// admin telegram id
+
+channelsList : ['@BinanceFutureSignal_Vip','@Legit_Bot_Update1','@Instant_Litecoin_Payment'], // channels list
+
+reffer_bonus: 0.00001000 ,// refferal bonus amount
+
+min_wd: 0.000001 ,//min withdrawal amount
+
+daily_bonus: 0.00000200,// daily bonus amount
+
+currency: 'LTC', //bot cuurency
+
+cb_api_key: 'O735bhxWudJwUmO4',//coinbase api key
+
+cb_api_secret: 'iXzwKnPlVTRYX16UDYaiR8LEsJzJYQAr',// coinbase secret key
+
+cb_account_id: '04976f94-3b80-5930-b793-8ba64ef2dc1d',// counbase account id for the bot currency
+
+payment_channel: '@Instant_Litecoin_Payment' // payment channel username
+
+}
+
