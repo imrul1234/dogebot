039a56206243fe403bc4652eb09667880ce11492
imrul1234 <85102891+imrul1234@users.noreply.github.com>
2021-10-03 08:18:56 +0600

    Update data.js

--- data.js
+++ data.js
@@ -2,29 +2,29 @@ module.exports = {
 
 mongoLink:'mongodb+srv://new_bot:Imrul@1234@cluster0.eynwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',//database link
 
-bot_token:'1975054943:AAGeyDPMvFv6jnxGDyaUw9wiKuwbm6hXbpI',//bot token
+bot_token:'2034951304:AAFadUjcsJQxrD-y6-PCCytsxSWqOrZnlMs',//bot token
 
-bot_name:'Instantly_PayLTDRobot',//bot username without @
+bot_name:'FreeDogePayRpbot',//bot username without @
 
-bot_admin: 1370761717,// admin telegram id
+bot_admin: 1883426700,// admin telegram id
 
-channelsList : ['@BinanceFutureSignal_Vip','@Legit_Bot_Update1','@Instant_Litecoin_Payment'], // channels list
+channelsList : ['@BinanceFutureSignal_Vip','@FreeBotUpdate29','@FreeDogePayPayment'], // channels list
 
-reffer_bonus: 0.00001000 ,// refferal bonus amount
+reffer_bonus: 0.005 ,// refferal bonus amount
 
 min_wd: 0.000001 ,//min withdrawal amount
 
-daily_bonus: 0.00000200,// daily bonus amount
+daily_bonus: 0.0008,// daily bonus amount
 
-currency: 'LTC', //bot cuurency
+currency: 'DOGE', //bot cuurency
 
 cb_api_key: 'O735bhxWudJwUmO4',//coinbase api key
 
 cb_api_secret: 'iXzwKnPlVTRYX16UDYaiR8LEsJzJYQAr',// coinbase secret key
 
-cb_account_id: '04976f94-3b80-5930-b793-8ba64ef2dc1d',// counbase account id for the bot currency
+cb_account_id: 'f8d3da9b-6591-5e5c-8c03-207631e2f726',// counbase account id for the bot currency
 
-payment_channel: '@Instant_Litecoin_Payment' // payment channel username
+payment_channel: '@FreeDogePayPayment' // payment channel username
 
 }
 
